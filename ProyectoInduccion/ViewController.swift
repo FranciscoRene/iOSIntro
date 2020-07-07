4cb7191ce828b53bcab1dd3ef03e42b7f8f07bbc
Francisco Flores <franciscorenef@gmail.com>
2020-07-07 09:47:39 -0400

    Initial Commit

---
+++ ProyectoInduccion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoInduccion
+//
+//  Created by Francisco Flores on 07-07-20.
+//  Copyright © 2020 FFF Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
