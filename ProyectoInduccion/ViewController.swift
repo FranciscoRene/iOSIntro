0033539e5587fd0602eb43cae953018640d47889
Francisco Flores <franciscorenef@gmail.com>
2020-07-07 11:44:46 -0400

    Creación vista login

--- ProyectoInduccion/ViewController.swift
+++ ProyectoInduccion/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtUser: UITextField!
+    @IBOutlet weak var txtPass: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        txtUser.backgroundColor = .cyan
+        txtPass.backgroundColor = .cyan
+    }
+    
+    //LoadView
+    override func loadView() {
+        super.loadView()
     }
 
 
