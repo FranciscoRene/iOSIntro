baab719f49a6b38962590b27658ba74114d65c11
Francisco Flores <franciscorenef@gmail.com>
2020-07-09 22:10:25 -0400

    Agregada validación del Login y vista Tabla+controlador

---
+++ ProyectoInduccion/MisVariables.swift
@@ -0,0 +1,11 @@
+//
+//  MisVariables.swift
+//  ProyectoInduccion
+//
+//  Created by Francisco Flores on 09-07-20.
+//  Copyright © 2020 FFF Software. All rights reserved.
+//
+
+import Foundation
+
+var listPais = ["Chile", "Peru", "Argentina"]
--- ProyectoInduccion/ViewController.swift
+++ ProyectoInduccion/ViewController.swift
@@ -8,16 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var txtUser: UITextField!
     @IBOutlet weak var txtPass: UITextField!
     
+    @IBAction func UserValidation(_ sender: Any) {
+        validarUsuario()
+    }
+    
+    func validarUsuario() {
+        if txtUser.text != "" {
+            print("hay usurio")
+            alertShowMessage(txtsms: "Hay Usuario")
+        }else {
+            alertShowMessage(txtsms: "Ingrese Usuario")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        txtUser.backgroundColor = .cyan
-        txtPass.backgroundColor = .cyan
+        txtUser.placeholder = "User"
+        txtPass.placeholder = "Pass"
+        
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if textField == txtUser {
+            
+            let returnTxtChar = textField.text?.count
+            
+            //! viene con algo
+            /*if returnTxtChar! > 6 {
+                alertShowMessage(txtsms: "bien de largo")
+            }else {
+                alertShowMessage(txtsms: "Error")
+            }*/
+            
+        }
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == txtUser {
+            txtUser.resignFirstResponder()
+            txtPass.becomeFirstResponder()
+        }
+        
+        if textField == txtPass {
+            txtPass.resignFirstResponder()
+            validarUsuario()
+        }
+        
+        print(textField.text ?? "")
+        
+        return false
     }
     
     //LoadView
@@ -25,6 +74,16 @@ class ViewController: UIViewController {
         super.loadView()
     }
 
+    func alertShowMessage (txtsms : String) {
+        let alert = UIAlertController(title : "Atención", message: txtsms, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
